7de598bbe6a47b5f8e947f80f5cd5a4a617cfd5b
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-10-19 15:46:14 +0000

    bootstrap boilerplate

--- src/webstruct-demo/__init__.py
+++ src/webstruct-demo/__init__.py
@@ -1,1 +1,8 @@
+from flask import Flask, render_template
 
+webstruct_demo = Flask(__name__, instance_relative_config=True)
+webstruct_demo.config.from_object('config')
+
+@webstruct_demo.route('/')
+def index():
+    return render_template('main.html')
