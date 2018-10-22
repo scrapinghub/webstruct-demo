43cdfc6efd46d493020c9e7f827d582e4ba0b560
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-10-22 12:07:59 +0000

    make every link absolute

--- src/webstruct-demo/__init__.py
+++ src/webstruct-demo/__init__.py
@@ -1,14 +1,45 @@
 from flask import Flask, render_template, request
 import lxml.html
 import requests
+import yarl
 
 
 webstruct_demo = Flask(__name__, instance_relative_config=True)
 webstruct_demo.config.from_object('config')
 
 
-def fix_hrefs(tree):
-    pass
+def absolute_links(tree, url):
+    _LINK_SOURCES = ['src', 'href']
+
+    try:
+        base_url = yarl.URL(url)
+    except:
+        return tree
+
+    for _, element in lxml.html.etree.iterwalk(tree, events=('start', )):
+        if not isinstance(element.tag, str):
+            continue
+
+        for attr in _LINK_SOURCES:
+            if attr not in element.attrib:
+                continue
+
+            try:
+                target_url = yarl.URL(element.attrib[attr])
+            except:
+                continue
+
+            if target_url.is_absolute():
+                continue
+
+            try:
+                target_url = base_url.join(target_url)
+            except:
+                continue
+
+            element.attrib[attr] = str(target_url)
+
+    return tree
 
 
 @webstruct_demo.route('/')
@@ -19,7 +50,8 @@ def index():
 
     parser = lxml.html.HTMLParser(encoding=response.encoding)
     tree = lxml.html.document_fromstring(response.content, parser=parser)
-    content = lxml.html.etree.tostring(tree)
+    tree = absolute_links(tree, url)
+    content = lxml.html.etree.tostring(tree).decode(response.encoding)
 
     values = {'url': url, 'output': output, 'iframe': content}
     return render_template('main.html', **values)
