9228bd1581f081589daf740296cc2ff81991dc0e
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-10-22 11:09:17 +0000

    process reponse content

--- src/webstruct-demo/__init__.py
+++ src/webstruct-demo/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import lxml.html
 import requests
 
 
@@ -6,11 +7,19 @@ webstruct_demo = Flask(__name__, instance_relative_config=True)
 webstruct_demo.config.from_object('config')
 
 
+def fix_hrefs(tree):
+    pass
+
+
 @webstruct_demo.route('/')
 def index():
     url = request.args.get('url', 'http://en.wikipedia.org/')
     output = request.args.get('output', 'html')
     response = requests.get(url)
-    content = response.text
+
+    parser = lxml.html.HTMLParser(encoding=response.encoding)
+    tree = lxml.html.document_fromstring(response.content, parser=parser)
+    content = lxml.html.etree.tostring(tree)
+
     values = {'url': url, 'output': output, 'iframe': content}
     return render_template('main.html', **values)
