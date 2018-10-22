2195a29bf80379e0bce97c6dd6cc3be9cad48482
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-10-22 10:59:38 +0000

    load modified page content in iframe

--- src/webstruct-demo/__init__.py
+++ src/webstruct-demo/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+import requests
+
 
 webstruct_demo = Flask(__name__, instance_relative_config=True)
 webstruct_demo.config.from_object('config')
+
 
 @webstruct_demo.route('/')
 def index():
     url = request.args.get('url', 'http://en.wikipedia.org/')
     output = request.args.get('output', 'html')
-    values = { 'url': url, 'output': output }
+    response = requests.get(url)
+    content = response.text
+    values = {'url': url, 'output': output, 'iframe': content}
     return render_template('main.html', **values)
