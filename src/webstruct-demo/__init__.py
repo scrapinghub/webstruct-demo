72794fc2b1e94b87639a06485fa89f2efd95886e
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-10-22 10:04:30 +0000

    value passing/preserving mechanism

--- src/webstruct-demo/__init__.py
+++ src/webstruct-demo/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 webstruct_demo = Flask(__name__, instance_relative_config=True)
 webstruct_demo.config.from_object('config')
 
 @webstruct_demo.route('/')
 def index():
-    return render_template('main.html')
+    url = request.args.get('url', 'http://en.wikipedia.org/')
+    output = request.args.get('output', 'html')
+    values = { 'url': url, 'output': output }
+    return render_template('main.html', **values)
